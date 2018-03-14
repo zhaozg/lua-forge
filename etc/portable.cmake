54d19cb00e43354c166a833799d8b3c98cfe7689
George Zhao <zhaozg@gmail.com>
2018-03-14 14:05:27 +0800

    add cmake build support

---
+++ etc/portable.cmake
@@ -0,0 +1,18 @@
+if(MSVC)
+  add_definitions( -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS )
+endif()
+
+if(UNIX)
+  add_definitions(-Wall)
+endif()
+
+if(APPLE)
+  set(CMAKE_EXE_LINKER_FLAGS "-pagezero_size 10000 -image_base 100000000 ${CMAKE_EXE_LINKER_FLAGS}")
+  set(CMAKE_SHARED_MODULE_CREATE_C_FLAGS
+    "${CMAKE_SHARED_MODULE_CREATE_C_FLAGS} -flat_namespace -undefined suppress"
+  )
+  # execute_process(COMMAND which luajit OUTPUT_VARIABLE LUAJIT)
+  # set(CMAKE_SHARED_MODULE_CREATE_C_FLAGS
+  #   "${CMAKE_SHARED_MODULE_CREATE_C_FLAGS} -bundle_loader ${LUAJIT}"
+  # )
+endif()
